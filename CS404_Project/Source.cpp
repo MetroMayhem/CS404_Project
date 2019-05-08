92ae5599a09d64e9a55dd4cff1bbe0c5a3c30049
Kory Overbay <kory.overbay@gmail.com>
2019-05-08 17:15:39 -0500

    Small efficiency update to vehicle matching algorithm

--- CS404_Project/Source.cpp
+++ CS404_Project/Source.cpp
@@ -14,13 +14,14 @@ using namespace std;
 int main() {
 	//Reading in files
 	ifstream vehiclesFile("Vehicles.txt");
-	vector<EmergencyVehicles> vehicles;
+	//vector<EmergencyVehicles> cars;
 	EmergencyVehicles vtemp;
+	map<int, vector<EmergencyVehicles>> vehicles;
 	while (!vehiclesFile.eof()) {
 		vehiclesFile >> vtemp.ID;
 		vehiclesFile >> vtemp.Type;
 		vehiclesFile >> vtemp.ZipCode;
-		vehicles.push_back(vtemp);
+		vehicles[vtemp.ZipCode].push_back(vtemp);
 	}
 	vehiclesFile.close();
 	ifstream requestsFile("Requests.txt");
@@ -37,31 +38,38 @@ int main() {
 	//Initializes graph of the data
 	List_Graph graph(V);
 	int* distances = new int[V];
-	int minZip = -1, VID = 0, minCost = INT_MAX;
+	int minZip = -1, minIdx = -1, VID = 0, minCost = INT_MAX;
 	Responded responded;
+	//vector<EmergencyVehicles>::iterator deleted;
 	for (vector<Request>::iterator req = requests.begin(); req != requests.end(); req++) {
 		//For each request, find the shortest cost path to each node with Dijkstras Algorithm
 		graph.dijkstras(graph.indices[req->get_zip()], distances);
 		for (int i = 0; i < V; i++) {
 			for (map<int, int>::iterator zip = graph.indices.begin(); zip != graph.indices.end(); zip++) {
 				//Iterates through the zipcode/index map to find which zip code matches
-				//to each cost
+				//to each cost, and if the indices match + that cost is less than the min
+				//See if the desired vehicle type is at that zip code
 				if (zip->second == i && distances[i] < minCost) {
-					for (vector<EmergencyVehicles>::iterator cars = vehicles.begin(); cars != vehicles.end(); cars++) {
-						if (zip->first == cars->ZipCode && cars->Type == req->get_vehicle_type()) {
+					//Iteratess through the emergency vehicles at the location from zip
+					//and sees if the desired vehicle is there, updates the cost if so
+					for (int car = 0; car < vehicles[zip->first].size(); car++) {
+						if (vehicles[zip->first][car].Type == req->get_vehicle_type()) {
 							minCost = distances[i];
 							minZip = zip->first;
-							VID = cars->ID;
+							VID = vehicles[zip->first][car].ID;
+							minIdx = car;
+							car = vehicles[zip->first].size();
 						}
 					}
-					zip = graph.indices.end();
-					zip--;
 				}
 			}
 		}
-		
+		//Adds the chosen vehicle to the responded table, moves the vehicle to the request zip code
+		//and resets the min vehicles variables
 		responded.AddResponded(req->get_request_id(), req->get_vehicle_type(), minZip, VID, minCost, req->get_zip());
-		vehicles[--VID].ZipCode = req->get_zip();
+		vehicles[req->get_zip()].push_back(vehicles[minZip][minIdx]);
+		(vehicles[req->get_zip()].end()-1)->ZipCode = req->get_zip();
+		vehicles[minZip].erase(vehicles[minZip].begin() + minIdx);
 		minZip = -1, VID = 0, minCost = INT_MAX;
 	}
 	cout << responded;
