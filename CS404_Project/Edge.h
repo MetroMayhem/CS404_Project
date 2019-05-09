f1c371fffb56dbeb584dc1f4fbced6ef8a1db5eb
Nathan Roberts <38477031+NRobot@users.noreply.github.com>
2019-05-08 22:10:26 -0500

    Comments

--- CS404_Project/Edge.h
+++ CS404_Project/Edge.h
@@ -6,29 +6,29 @@ class Edge {
 
 public:
 
+	// Constructors
 	Edge(int firstZip, int secondZip, int dist);
 	Edge(const Edge& other);
 	Edge();
 
 	bool operator==(Edge& other);
+	
+	// Getters
 	int getZip1();
 	int getZip2();
 	int getDistance();
 
+	// Setters
 	void setZip1(int z1);
 	void setZip2(int z2);
 	void setDistance(int d);
 
 	friend std::ostream& operator<<(std::ostream& os, Edge& edge);
 
-
 private:
-
 
 	int zipCode1;
 	int zipCode2;
-	int distance;
-
+	int distance;  // Weight of each edge
 
 };
-
